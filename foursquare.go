112505621da1adaf42f2dc2c630afad5af22bcbe
Roman Tomjak <6570684+romantomjak@users.noreply.github.com>
2018-12-01 15:16:03 +0000

    serialize foursquare API responses, woohoo!

--- foursquare.go
+++ foursquare.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "encoding/json"
     "io/ioutil"
     "net/http"
     "time"
@@ -14,6 +15,28 @@ type FoursquareClient struct {
     baseURL string
 }
 
+type response struct {
+    Response json.RawMessage `json:"response"`
+}
+
+type groupList struct {
+    Groups []group `json:"groups"`
+}
+
+type groupItem struct {
+    Venue Venue `json:"venue"`
+}
+
+type group struct {
+    Type  string `json:"type"`
+    Name  string `json:"name"`
+    Items []groupItem `json:"items"`
+}
+
+type Venue struct {
+    Name string `json:"name"`
+}
+
 func NewFoursquareClient(clientId string, secret string) *FoursquareClient {
     return &FoursquareClient{
         clientId: clientId,
@@ -22,13 +45,32 @@ func NewFoursquareClient(clientId string, secret string) *FoursquareClient {
     }
 }
 
-func (fc *FoursquareClient) VenuesNearby(location string) string {
+func (fc *FoursquareClient) VenuesNearby(location string) []Venue {
     body, err := fc.query("venues/explore/?near=" + location + "&query=lunch")
     if err != nil {
         log.Error("Failed to query Foursquare Venues")
-        return ""
+        return nil
     }
-    return string(body)
+
+    r := response{}
+    err = json.Unmarshal(body, &r)
+    if err != nil {
+        log.Error("Failed to deserialize API response")
+        return nil
+    }
+
+    gl := &groupList{}
+    err = json.Unmarshal(r.Response, &gl)
+    if err != nil {
+        log.Error("Failed to deserialize API response")
+        return nil
+    }
+
+    venues := []Venue{}
+    for i := range gl.Groups[0].Items {
+        venues = append(venues, gl.Groups[0].Items[i].Venue)
+    }
+    return venues
 }
 
 func (fc *FoursquareClient) query(reqPath string) (response []byte, err error) {
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "encoding/json"
     "os"
     "fmt"
     "net/http"
@@ -16,6 +17,11 @@ func getEnv(key, fallback string) string {
 }
 
 func lunchIdeas(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        http.Error(w, "404 not found.", http.StatusNotFound)
+        return
+    }
+
     location := os.Getenv("LOCATION")
     client_id := os.Getenv("CLIENT_ID")
     secret := os.Getenv("SECRET")
@@ -23,11 +29,18 @@ func lunchIdeas(w http.ResponseWriter, r *http.Request) {
     fc := NewFoursquareClient(client_id, secret)
     venues := fc.VenuesNearby(location)
 
-    fmt.Fprintf(w, venues)
+    json, err := json.Marshal(venues)
+    if err != nil {
+        log.Error("Failed to serialize JSON response")
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(json)
 }
 
 func main() {
-    addr := fmt.Sprintf(":%s", getEnv("PORT", "8080"))
+    addr := fmt.Sprintf("127.0.0.1:%s", getEnv("PORT", "8080"))
     log.Infof("Starting server on %s", addr)
     http.HandleFunc("/", lunchIdeas)
     log.Fatal(http.ListenAndServe(addr, nil))
